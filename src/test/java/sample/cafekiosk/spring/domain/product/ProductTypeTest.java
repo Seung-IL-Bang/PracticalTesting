9a60f9f10dfc9953c0660e39fd31d7ef972acd13
Seungil <seungilbang@khu.ac.kr>
2023-07-08 21:32:48 +0900

    [UPD] Section 7 : 한 문단에 한 주제!

--- src/test/java/sample/cafekiosk/spring/domain/product/ProductTypeTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/product/ProductTypeTest.java
@@ -3,11 +3,40 @@ package sample.cafekiosk.spring.domain.product;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductTypeTest {
 
+    // 모든 주제 한 꺼번에 테스트
+    @DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
+    @Test
+    void containsStockTypeEx() {
+        //given
+        ProductType[] productTypes = ProductType.values();
+
+        for (ProductType productType : productTypes) {
+            if (productType == ProductType.HANDMADE) {
+                // when
+                boolean result = ProductType.containsStockType(productType);
+
+                // then
+                assertThat(result).isFalse();
+            }
+
+            if (productType == ProductType.BAKERY || productType == ProductType.BOTTLE) {
+                // when
+                boolean result = ProductType.containsStockType(productType);
+
+                // then
+                assertThat(result).isTrue();
+            }
+        }
+
+    }
+
+    // 한 주제당 한 개씩 테스트
     @DisplayName("상품 타입이 재고 관련 타입이 아닌지를 체크한다.")
     @Test
     void containsStockType() {
@@ -22,6 +51,7 @@ class ProductTypeTest {
 
     }
 
+    // 한 주제당 한 개씩 테스트
     @DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
     @Test
     void containsStockType2() {
